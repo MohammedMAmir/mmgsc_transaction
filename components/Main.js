1416139f79e7c7c4a62c76030fa6461c894c8cff
Mohammed <muji-amir@hotmail.com>
2025-04-12 16:45:58 -0400

    dashboard layout

--- components/Main.js
+++ components/Main.js
@@ -7,8 +7,15 @@ export default function Main(props) {
     // Unpack any props passed to this component
     const { children } = props
     return (
-    <main className='flex-1 flex flex-col p-4 sm:p-8'>
-        { children } 
+    <main className='flex-1 flex flex-col bg-[var(--body-gray)]'>
+        <div className='p-2 sm:p-4 grid grid-cols-1 sm:grid-cols-6'>
+            <div className='col-span-1'>
+                Settings
+            </div>
+            <div className='col-span-5'>
+            { children }
+            </div>  
+        </div>
     </main>
     )
 }
