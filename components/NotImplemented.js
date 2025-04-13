403ec26e5c5b05a3683d415228339232d0a90ce8
Mohammed Amir <muji-amir@hotmail.com>
2025-04-12 23:27:15 -0400

    starting work on the button

--- components/NotImplemented.js
+++ components/NotImplemented.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NotImplemented() {
   return (
-    <div className="p-2 sm:p-4 flex items-center justify-between gap-4" >
+    <div className="p-10 sm:p-8 flex items-center justify-between gap-4 " >
       <h1 className="text-sm lg:text-base">Not Implemented</h1>
     </div>
   )
